59a4123ded646caf67d3e79b0bbd8d4af893b370
Patrick Obermueller <pat.obermueller@gmail.com>
2024-12-15 15:46:39 +0100

    move back main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,16 @@ Devvit.configure({
 	// realtime: true,
 })
 
-import './blocks/addAppSettings'
-import './blocks/addMenuItem'
-import './jobs/dailyJob'
-import './jobs/firstFlapperComment'
-import './jobs/newHighscoreComment'
-import './jobs/welcomeUser'
-import './triggers/daily'
+import './app/blocks/addAppSettings'
+import './app/blocks/addMenuItem'
+import './app/jobs/dailyJob'
+import './app/jobs/firstFlapperComment'
+import './app/jobs/newHighscoreComment'
+import './app/jobs/welcomeUser'
+import './app/triggers/daily'
 
-import { SplashScreen } from './SplashScreen'
-import { WebviewContainer } from './WebviewContainer'
+import { SplashScreen } from './app/SplashScreen'
+import { WebviewContainer } from './app/WebviewContainer'
 
 Devvit.addCustomPostType({
 	name: `Let's play Reddibirds!`,
